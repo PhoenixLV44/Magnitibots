edaf9f3eab89a8378368b1b8a8d8a312e06a8df2
Lexi <alexandriavye44@gmail.com>
2026-05-28 19:42:39 -0400

    Created Skeleton for state machine

---
+++ MagnetiBots/Assets/State Machine/Ground State/GroundedState.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Player;
+
+namespace Player
+{
+    public class GroundedState : Player.PlayerState
+    {
+        //public PlayerGroundedSuperState(PlayerAbility pc, string animationName, PlayerStateMachine stateMachine) : base(pc, animationName, stateMachine) { }
+    }
+}
---
+++ MagnetiBots/Assets/State Machine/PlayerState.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using Player;
+namespace Player
+{
+    public class PlayerState
+    {
+        protected Player.PlayerAbility player; //might need to change later
+        protected Player.PlayerStateMachine stateMachine;
+        protected Animator animationController;
+        protected string animationName;
+
+        protected bool isExitingState;
+        protected bool isAnimationFinished;
+        protected float startTime;
+
+        public PlayerState(Player.PlayerAbility _player, PlayerStateMachine _stateMachine, Animator _animationController, string _animationName)
+        {
+            player = _player;
+            stateMachine = _stateMachine;
+            animationController = _animationController;
+            animationName = _animationName;
+        }
+        public virtual void EnterState()
+        {
+            isAnimationFinished = false;
+            isExitingState = false;
+            startTime = Time.time;
+            animationController.SetBool(animationName, true);
+        }
+        public virtual void ExitState()
+        {
+            isExitingState = true;
+            if (!isAnimationFinished) isAnimationFinished = true;
+            animationController.SetBool(animationName, false);
+        }
+        public virtual void LogicUpdate()
+        {
+            TransitionChecks();
+        }
+        public virtual void PhysicsUpdate()
+        {
+
+        }
+        public virtual void TransitionChecks()
+        {
+
+        }
+        public virtual void AnimationTrigger()
+        {
+            isAnimationFinished = true;
+        }
+    }
+}
