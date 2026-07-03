99f689614f9eae87a86232bf8995f22a5484653b
Lexi <alexandriavye44@gmail.com>
2026-07-03 14:48:48 -0400

    Check for Global before calling ability popup

--- MagnetiBots/Assets/Scripts/PlayerCharacter/PlayerController.cs
+++ MagnetiBots/Assets/Scripts/PlayerCharacter/PlayerController.cs
@@ -204,20 +204,27 @@ namespace Player
             if (!canUseLasso)
             {
                 Debug.Log("can use lasso");
-                Globals.Managers.Settings.UnlockPopup("Lasso");
+                if (FindFirstObjectByType<Globals>() != null)
+                {
+                    Globals.Managers.Settings.UnlockPopup("Lasso");
+                }
                 canUseLasso = true;
             }
             else if (!canUseSmash)
             {
                 Debug.Log("can use smash");
-                Globals.Managers.Settings.UnlockPopup("Smash");
-                canUseSmash = true;
+                if (FindFirstObjectByType<Globals>() != null)
+                {
+                    Globals.Managers.Settings.UnlockPopup("Smash");
+                }                canUseSmash = true;
             }
             else if (!canUseSuperJump)
             {
                 Debug.Log("can use super jump");
-                Globals.Managers.Settings.UnlockPopup("SuperJump");
-                canUseSuperJump = true;
+                if (FindFirstObjectByType<Globals>() != null)
+                {
+                    Globals.Managers.Settings.UnlockPopup("SuperJump");
+                }                canUseSuperJump = true;
             }
             _animator.Play("Collect");
         }
